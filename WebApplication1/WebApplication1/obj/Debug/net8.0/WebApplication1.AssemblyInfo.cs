bef708b656ecad6fff6bc823f6dfc77229b7e143
Rickhoutzager <159452984+Rickhoutzager@users.noreply.github.com>
2025-03-18 11:43:05 +0100

    gitignore

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6fbfbc095703e3049207f98462a863f1a85f2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2f88817423d2f5fff2150f7d808ce102796373")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
