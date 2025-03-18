24928f250bb306dbe8cba55f48ddb9a1a3ea390d
Nikki440 <s1198053@student.windesheim.nl>
2025-03-18 13:58:14 +0100

    models setup

---
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication1.Models
+{
+    public class Animal
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime Birthday { get; set; }
+        public speciesEnum species { get; set; }
+        public dietEnum diet { get; set; }
+    }
+
+    public enum dietEnum
+    {
+        Carnovore,
+        Herbivore,
+        Omnivore
+    }
+    public enum speciesEnum
+    {
+        Fish,
+        Mammels,
+        Birds,
+        Reptiles,
+        Amphibians
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Enclosure.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Models
+{
+    public class Enclosure
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string animal { get; set; }
+        public habitatSizeEnum habitatSize { get; set; }
+        public habitatWeatherEnum habitatweather { get; set; }
+    }
+    public enum habitatSizeEnum
+    {
+        Low,
+        Medium,
+        High
+    }
+    public enum habitatWeatherEnum
+    {
+        Sunny,
+        Cloudy,
+        Raining,
+        Clear
+    }
+}
