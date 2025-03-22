6de5dace55f5342bffc3140487ccc56e9d455a5b
TechRH1 <159452984+Rickhoutzager@users.noreply.github.com>
2025-03-22 15:06:15 +0100

    Added enclosure index page and added enclosures to navbar

---
+++ WebApplication1/WebApplication1/Controllers/EnclosureController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class EnclosureController : Controller
+    {
+        private readonly DBContextDatabase _context;
+
+        public EnclosureController(DBContextDatabase context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var enclosures = await _context.Enclosures.ToListAsync();
+            return View(enclosures);
+        }
+    }
+}
