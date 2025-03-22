97eecc440d674bba41124d55e70f47c04aa9b640
TechRH1 <159452984+Rickhoutzager@users.noreply.github.com>
2025-03-22 15:25:42 +0100

    Added animal category index page and added categories to navbar

---
+++ WebApplication1/WebApplication1/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DBContextDatabase _context;
+
+        public CategoryController(DBContextDatabase context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
