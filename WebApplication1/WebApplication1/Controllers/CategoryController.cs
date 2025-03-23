46fc887bd1897eaed6e803a691c65d61a134aaea
Nikki <s1198053@student.windesheim.nl>
2025-03-23 10:54:03 +0100

    CategoryController CRUD functions

--- WebApplication1/WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/WebApplication1/Controllers/CategoryController.cs
@@ -1,23 +1,111 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
-using System.Threading.Tasks;
 
-namespace WebApplication1.Controllers
+public class CategoryController : Controller
 {
-    public class CategoryController : Controller
+    private readonly DBContextDatabase _context;
+
+    public CategoryController(DBContextDatabase context)
     {
-        private readonly DBContextDatabase _context;
+        _context = context;
+    }
 
-        public CategoryController(DBContextDatabase context)
+    // GET: Category
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _context.Categories.ToListAsync();
+        return View(categories);
+    }
+
+    // GET: Category/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Category/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Name")] Category category)
+    {
+        if (ModelState.IsValid)
         {
-            _context = context;
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+
+    // GET: Category/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
         }
 
-        public async Task<IActionResult> Index()
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
         {
-            var categories = await _context.Categories.ToListAsync();
-            return View(categories);
+            return NotFound();
         }
+        return View(category);
+    }
+
+    // POST: Category/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
+    {
+        if (id != category.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(category.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+
+    // POST: Category/Delete/5
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool CategoryExists(int id)
+    {
+        return _context.Categories.Any(e => e.Id == id);
     }
 }
