05c4571df0d310fb813fd2dc139e855ceb6943b5
Nikki <s1198053@student.windesheim.nl>
2025-03-23 14:38:13 +0100

    Edit Enclosure
    Razor kreeg ik niet werkent voor nu wordt javascript gebruikt

--- WebApplication1/WebApplication1/Controllers/EnclosureController.cs
+++ WebApplication1/WebApplication1/Controllers/EnclosureController.cs
@@ -40,6 +40,57 @@ namespace WebApplication1.Controllers
             return View(enclosure);
         }
 
+        // GET: Enclosure/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enclosure = await _context.Enclosures.FindAsync(id);
+            if (enclosure == null)
+            {
+                return NotFound();
+            }
+
+            return View(enclosure);
+        }
+
+        // POST: Enclosure/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Climate, HabitatType, SecurityLevel, Size")] Enclosure enclosure)
+        {
+            if (id != enclosure.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(enclosure);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Enclosures.Any(e => e.Id == enclosure.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(enclosure);
+        }
+
+
         // POST: Enclosure/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
